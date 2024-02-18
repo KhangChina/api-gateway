88c573a4599a649bbd5b2ff8e7beff8dca1edb64
Khang Nguyen <khang.nguyen@htgsoft.com>
2024-02-18 17:04:21 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,45 @@
-import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService) {}
   @Get(':services/*')
-  async findAllData(@Param('services') services: string,@Param('endpoint') endpoint: string) {
-    console.log(services)
-    console.log(endpoint[0])
-    return await this.appService.findAll()
-  }
+  async getData(@Param('services') services: string,@Req() request: Request) {
+    //Step 1: Get services
+    const servicesAPI =  services;
+    //Step 2: Get domain api for service
+    let domain = ""
+    if(servicesAPI=== "myapi")
+    {
+      domain = "https://637a547a10a6f23f7f91af54.mockapi.io"
+    }
+    //Step 3: Create domain for api
+    const url = request.url
+    let newUrl = url.replace(`/${servicesAPI}`, domain)
+    const response = await this.httpService.axiosRef.request({
+      method: request.method,
+      url: newUrl,
+     // headers: request.headers,
+      data: request.body
+    });
+    return response.data;
 
-  @Post(':services')
-  getPosts(): string {
-    return "Post"
+   // console.log(newUrl)
+    //Step 4: Transfer API for services 
+    //return await this.appService.findAll(newUrl);
   }
+  // @Post(':services')
+  // async postData(): {
+  //   return "Post"
+  // }
 
-  @Patch(':services')
-  getHelloss(): string {
-    return "Hello"
-  }
+  // @Patch(':services')
+  // async getHelloss(): string {
+  //   return "Hello"
+  // }
+  
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,19 +2,33 @@ import { HttpService } from '@nestjs/axios';
 import { Dependencies, Injectable } from '@nestjs/common';
 
 @Injectable()
-@Dependencies(HttpService)
 export class AppService {
-  httpService: any;
-  constructor(httpService) {
-    this.httpService = httpService;
+  constructor(private readonly httpService: HttpService) {
   }
 
   getHello(): string {
     return 'Hello World!';
   }
 
-  findAll() {
-    return this.httpService.get('https://637a547a10a6f23f7f91af54.mockapi.io/woo/user');
+  async findAll(domain) {
+    const data = await this.httpService.axiosRef.get('https://637a547a10a6f23f7f91af54.mockapi.io/woo/user',);
+    return data.data;
+  }
+
+  create(createGatewayDto: any) {
+    return 'This action adds a new gateway';
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} gateway`;
+  }
+
+  update(id: number, updateGatewayDto: any) {
+    return `This action updates a #${id} gateway`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} gateway`;
   }
 
 }
